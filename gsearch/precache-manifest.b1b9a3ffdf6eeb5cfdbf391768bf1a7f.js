13d51a6e0be8f6693bb7e3f1298c499a69e5e0d5
jzlikewei <jzlikewei@163.com>
2020-06-06 13:16:46 +0800

    add gsearch

---
+++ gsearch/precache-manifest.b1b9a3ffdf6eeb5cfdbf391768bf1a7f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdeb46077740cebf37c05e17f5243277",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9cee1c61404b98468949",
+    "url": "/static/css/2.4deadad5.chunk.css"
+  },
+  {
+    "revision": "ef2eb351d6bdb7d13453",
+    "url": "/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "9cee1c61404b98468949",
+    "url": "/static/js/2.a8e0063a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.a8e0063a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef2eb351d6bdb7d13453",
+    "url": "/static/js/main.acf5806b.chunk.js"
+  },
+  {
+    "revision": "1ff00dea7edd7ab560c4",
+    "url": "/static/js/runtime-main.6d5ab6a2.js"
+  }
+]);
